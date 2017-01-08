fd5ee60d36f15c8619ef869abd70147734479f42
ictrobot <ictrobot@outlook.com>
2017-01-08 22:22:26 +0000

    Multiple expressions, difference generator, update readme

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Cubes Equation Terrain Generator
 
-When using this mod, select "Equation" from the terrain generator drop down, and enter an equation using x and/or z into the seed textbox
+When using this mod, select "Equation" from the terrain generator drop down, and enter an equation using x and/or z into the seed textbox.
 
-[exp4j](http://www.objecthunter.net/exp4j/) is used to evaluate the equation
+[exp4j](http://www.objecthunter.net/exp4j/) is used to evaluate the equation.
+
+## Terrain Generators
+The vertical bar ( | ) symbol can be used to enter multiple equations. The terrain is generated using a block which changes colour every 5 blocks to show height difference clearly. 
+
+**Equation** generator will evaluate each equation and place a block at the y value returned
+
+**Equation Filled** generator will evaluate each equation and fill from the highest y valued returned to 0
+
+**Equation Difference** generator will evaluate each equation and fill from the lowest returned y value to the highest returned value
+
+## Examples
+ - Sphere: ```50-sqrt(-x^2-z^2+2500) | 50+sqrt(-x^2-z^2+2500)``` (best on Equation Difference)
+ - Ripples ```20sin(sqrt(x^2 + z^2) * 0.05) + 40```
+ - Hills: ```25sin(x * 0.02) + 25sin(z * 0.02) + 50```
+
+## Images
+![Image 1](/images/readme1.png)
+![Image 2](/images/readme2.png)
+![Image 3](/images/readme3.png)
--- src/ethanjones/cubes/equationterraingenerator/EquationTerrainGenerator.java
+++ src/ethanjones/cubes/equationterraingenerator/EquationTerrainGenerator.java
@@ -19,6 +19,8 @@ import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.ValidationResult;
 
+import java.util.regex.Pattern;
+
 @Mod
 public class EquationTerrainGenerator {
   private static Block block;
@@ -29,87 +31,158 @@ public class EquationTerrainGenerator {
     GeneratorManager.register("equationterraingenerator:equation", new TerrainGeneratorFactory() {
       @Override
       public TerrainGenerator getTerrainGenerator(SaveOptions saveOptions) {
-        return new Generator(saveOptions.worldSeedString, false);
+        return new NormalGenerator(saveOptions.worldSeedString);
       }
     });
     GeneratorManager.register("equationterraingenerator:equationfilled", new TerrainGeneratorFactory() {
       @Override
       public TerrainGenerator getTerrainGenerator(SaveOptions saveOptions) {
-        return new Generator(saveOptions.worldSeedString, true);
+        return new FilledGenerator(saveOptions.worldSeedString);
+      }
+    });
+    GeneratorManager.register("equationterraingenerator:equationdifference", new TerrainGeneratorFactory() {
+      @Override
+      public TerrainGenerator getTerrainGenerator(SaveOptions saveOptions) {
+        return new DifferenceGenerator(saveOptions.worldSeedString);
       }
     });
   }
   
-  public static class Generator extends TerrainGenerator {
-    private final String eqn;
-    private final boolean filled;
-    private ThreadLocal<Expression> expression = new ThreadLocal<Expression>() {
+  public static abstract class Generator extends TerrainGenerator {
+    protected final String[] eqn;
+    protected ThreadLocal<Expression[]> expressionThreadLocal = new ThreadLocal<Expression[]>() {
       @Override
-      protected Expression initialValue() {
-        return getExpression();
+      protected Expression[] initialValue() {
+        Expression[] expressions = new Expression[eqn.length];
+        for (int i = 0; i < eqn.length; i++) {
+          expressions[i] = getExpression(eqn[i]);
+        }
+        return expressions;
       }
     };
     
-    public Generator(String eqn, boolean filled) {
-      this.eqn = eqn;
-      this.filled = filled;
-      Log.info("Equation " + (filled ? "Filled " : "") + "Terrain Generator: " + eqn);
+    public Generator(String eqn) {
+      Log.info("Equation Terrain " + getClass().getSimpleName() + ": " + eqn);
+      this.eqn = eqn.split(Pattern.quote("|"));
       try {
-        ValidationResult validationResult = getExpression().validate(false);
-        if (!validationResult.isValid()) throw new RuntimeException(String.valueOf(validationResult.getErrors()));
+        for (Expression expression : expressionThreadLocal.get()) {
+          ValidationResult validationResult = expression.validate(false);
+          if (!validationResult.isValid()) throw new RuntimeException("Invalid expression " + String.valueOf(validationResult.getErrors()));
+        }
       } catch (Exception e) {
-        Debug.crash(e);
+        Debug.crash(new RuntimeException("Invalid expression", e));
       }
     }
+    
+    protected void block(Area area, int x, int y, int z) {
+      int meta = (y / 5) % 41;
+      if (meta > 20) meta = 40 - meta;
+      set(area, block, x, y, z, meta);
+    }
+    
+    @Override
+    public void features(Area area, WorldServer world) {
+      
+    }
+    
+    @Override
+    public BlockReference spawnPoint(WorldServer world) {
+      int y = -1;
+      for (Expression expression : expressionThreadLocal.get()) {
+        int i = height(expression, 0, 0);
+        if (i > y) y = i;
+      }
+      if (y == -1) return new BlockReference().setFromBlockCoordinates(0, 100, 0);
+      return new BlockReference().setFromBlockCoordinates(0, y + 2, 0);
+    }
+    
+    protected Expression getExpression(String eqn) {
+      return new ExpressionBuilder(eqn).variable("x").variable("z").build();
+    }
+    
+    protected int height(Expression e, int x, int z) {
+      try {
+        e.setVariable("x", x);
+        e.setVariable("z", z);
+        int evaluate = (int) e.evaluate();
+        return evaluate < 0 ? -1 : evaluate;
+      } catch (Exception exception) {
+        return -1;
+      }
+    }
+  }
   
+  public static class NormalGenerator extends Generator {
+    
+    public NormalGenerator(String eqn) {
+      super(eqn);
+    }
+    
     @Override
     public void generate(Area area) {
+      Expression[] expressions = expressionThreadLocal.get();
       for (int x = 0; x < Area.SIZE_BLOCKS; x++) {
         for (int z = 0; z < Area.SIZE_BLOCKS; z++) {
-          int y = height(x + area.minBlockX, z + area.minBlockZ);
-          if (filled) {
-            for (int i = y; i >= 0; i--) {
-              block(area, x, i, z);
-            }
-          } else {
+          for (Expression e : expressions) {
+            int y = height(e, x + area.minBlockX, z + area.minBlockZ);
             if (y != -1) block(area, x, y, z);
           }
         }
       }
     }
     
-    private void block(Area area, int x, int y, int z) {
-      int meta = (y / 5) % 41;
-      if (meta > 20) meta = 40 - meta;
-      setVisible(area, block, x, y, z, meta);
-    }
+  }
   
+  public static class FilledGenerator extends Generator {
+    
+    public FilledGenerator(String eqn) {
+      super(eqn);
+    }
+    
     @Override
-    public void features(Area area, WorldServer world) {
-    
-    }
-  
-    @Override
-    public BlockReference spawnPoint(WorldServer world) {
-      int y = height(0, 0);
-      if (y == -1) return new BlockReference().setFromBlockCoordinates(0, 100, 0);
-      return new BlockReference().setFromBlockCoordinates(0, y + 2, 0);
-    }
-    
-    private Expression getExpression() {
-      return new ExpressionBuilder(eqn).variable("x").variable("z").build();
-    }
-    
-    private int height(int x, int z) {
-      try {
-        Expression e = expression.get();
-        e.setVariable("x", x);
-        e.setVariable("z", z);
-        int evaluate = (int) e.evaluate();
-        return evaluate < 0 ? -1 : evaluate;
-      } catch (Exception e) {
-        return -1;
+    public void generate(Area area) {
+      Expression[] expressions = expressionThreadLocal.get();
+      for (int x = 0; x < Area.SIZE_BLOCKS; x++) {
+        for (int z = 0; z < Area.SIZE_BLOCKS; z++) {
+          int minHeight = 0;
+          for (Expression e : expressions) {
+            int y = height(e, x + area.minBlockX, z + area.minBlockZ);
+            for (int i = y; i >= minHeight; i--) {
+              block(area, x, i, z);
+            }
+            if (y > minHeight) y = minHeight + 1;
+          }
+        }
       }
     }
+    
+  }
+  
+  public static class DifferenceGenerator extends Generator {
+    
+    public DifferenceGenerator(String eqn) {
+      super(eqn);
+    }
+    
+    @Override
+    public void generate(Area area) {
+      Expression[] expressions = expressionThreadLocal.get();
+      for (int x = 0; x < Area.SIZE_BLOCKS; x++) {
+        for (int z = 0; z < Area.SIZE_BLOCKS; z++) {
+          int minHeight = -1, maxHeight = -1;
+          for (Expression e : expressions) {
+            int y = height(e, x + area.minBlockX, z + area.minBlockZ);
+            if (y != -1 && (minHeight == -1 || y < minHeight)) minHeight = y;
+            if (y > maxHeight) maxHeight = y;
+          }
+          if (minHeight != -1) {
+            for (int y = minHeight; y <= maxHeight; y++) {
+              block(area, x, y, z);
+            }
+          }
+        }
+      }
+    }
+    
   }
 }
